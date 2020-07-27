19ead32b7447c35c603ca67f0a2fbf0f8953f5b5
Alvaro VP <jhonalvarovallejosperez@gmail.com>
2020-07-27 19:00:35 -0400

    Create listar.php

---
+++ listar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Listar Documentos</title>
+</head>
+<body>
+<center><h1>Listado de Documentos Para Realizar Prestamo de la Biblioteca </h1></center>
+<table border="1" align="center">
+	<tr>
+		<td align="center">CI</td>
+		<td><img src="ci.jpg" height="150" width="200"></td>
+	</tr>
+	<tr>
+		<td align="center">MATRICULA</td>
+		<td><img src="matricula.jpg" height="150" width="200"></td>
+	</tr>
+	<tr>
+		<td align="center">NOMBRE DEL LIBRO</td>
+		<td><img src="libro.jpg" height="150" width="200"></td>
+	</tr>
+	<tr>
+		<td align="center">CODIGO DEL LIBRO</td>
+		<td><img src="libro.jpg" height="150" width="200"></td>
+	</tr>
+</table>
+</body>
+</html>
+
